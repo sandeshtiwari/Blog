0f01bcf5212681f85975317dff7e08bd24ab3e7f
Sandesh Tiwari <sandeshtiwari@live.com>
2018-06-19 17:27:44 -0500

    feat: Add Comment class to correspont to a post

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -15,7 +15,15 @@ class Post(models.Model):
         self.save()
 
     def approve_comments(self):
-        return self.comments.filter(approved_comments=True)
+        return self.comments.filter(approved_comment=True)
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post',related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    approved_comment = models.BooleanField(default=False)
