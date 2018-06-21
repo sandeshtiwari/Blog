3c73932c4578f9de5d4446b314391dd6b6d224a0
Sandesh Tiwari <sandeshtiwari@live.com>
2018-06-21 09:00:32 -0500

    feat: Add DraftListView listview Also add get_queryset method to get the Posts that have published_date as null

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -35,3 +35,11 @@ class PostUpdateView(LoginRequiredMixin, UpdateView):
 class PostDeleteView(LoginRequiredMixin, DeleteView):
     model = Post
     success_url = reverse_lazy('post_list')
+
+class DraftListView(LoginRequiredMixin, ListView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_list.html'
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('create_date')
