c6e5cea433316c23a3a0fd60df216e15bca1da3a
Sandesh Tiwari <sandeshtiwari@live.com>
2018-06-21 08:47:45 -0500

    feat: Add PostUpdateView view with respective url

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('/about', views.AboutView.as_view(), name='about'),
     path('post/<int:pk>', views.PostDetailView.as_view(), name='post_detail'),
     path('post/new', views.CreatePostView.as_view(), name='post_view'),
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,7 +3,7 @@ from blog.models import Post, Comment
 from blog.forms import PostForm, CommentForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (TemplateView, ListView
-                                    DetailView, CreateView)
+                                    DetailView, CreateView, UpdateView)
 # Create your views here.
 
 class AboutView(TemplateView):
@@ -19,6 +19,12 @@ class PostDetailView(DetailView):
     model = Post
 
 class CreatePostView(LoginRequiredMixin,CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
     login_url = '/login/'
     redirect_field_name = 'blog/post_detail.html'
     form_class = PostForm
