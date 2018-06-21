45888e5b3c5d4e12cdaeeec7fb7e4301415be63b
Sandesh Tiwari <sandeshtiwari@live.com>
2018-06-21 08:39:12 -0500

    feat: Add CBVs and urls

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
+    path('',views.PostListView.as_view(), name='post_list'),
     path('/about', views.AboutView.as_view(), name='about'),
+    path('post/<int:pk>', views.PostDetailView.as_view(), name='post_detail'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-from django.views.generic import (TemplateView)
+from blog.models import Post, Comment
+from django.views.generic import (TemplateView, ListView
+                                    DetailView, CreateView)
 # Create your views here.
 
 class AboutView(TemplateView):
     template_name = 'about.html'
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+class PostDetailView(DetailView):
+    model = Post
+
+class CreatePostView(CreateView):
+    model = Post
