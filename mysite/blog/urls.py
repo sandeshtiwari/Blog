059bc19d082737e504479f879bd11bf44e371f8c
Sandesh Tiwari <sandeshtiwari@live.com>
2018-06-22 14:51:58 -0500

    feat: Add comment approve view

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
     path('post/<int:pk>/remove', views.PostDeleteView.as_view(), name='post_remove'),
     path('drafts/', views.DraftListView.as_view(), name='post_draft_list'),
+    path('post/<int:pk>/comment/', views.AboutView, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/',views.comment_approve, name='comment_approve'),
 ]
