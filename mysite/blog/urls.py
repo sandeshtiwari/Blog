f1906cf15a2f9d9f287e2fc65d0172a58175e775
Sandesh Tiwari <sandeshtiwari@live.com>
2018-06-22 14:57:47 -0500

    feat: Add post publish view

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('post/<int:pk>/comment/', views.AboutView, name='add_comment_to_post'),
     path('comment/<int:pk>/approve/',views.comment_approve, name='comment_approve'),
     path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
+    path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
 ]
